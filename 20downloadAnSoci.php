d00dfd28c22924d1d9a1efb19f38c08848e4663d
nicolacardi <nicola.cardi@gmail.com>
2022-09-25 12:17:30 +0200

    download generico

--- 20downloadAnSoci.php
+++ 20downloadAnSoci.php
@@ -1,59 +1,70 @@
-<?	require_once('vendor/autoload.php');
+<?	
+
+	//INPUT
+	$title = "AnagraficaSoci";
+	$nomiCampiA = array("idle", "descrizione_tsc", "dataiscrizione_soc", "cognome_soc", "nome_soc", "telefono_soc", "altrotel_soc", "email_soc", "datanascita_soc", "comunenascita_soc", "provnascita_soc", "paesenascita_soc", "cf_soc", "indirizzo_soc", "comune_soc", "CAP_soc", "prov_soc", "paese_soc");
+	$from = " tab_anagraficasoci LEFT JOIN tab_tipisoci ON tipo_soc = ID_tsc ";
+	$orderBY = "cognome_soc ";
+	$dataNonDataA = array("idle", 0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0);
+
+
+	require_once('vendor/autoload.php');
 	use PhpOffice\PhpSpreadsheet\Spreadsheet;
 	use PhpOffice\PhpSpreadsheet\IOFactory;
-	$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load("TemplateExportAnagraficaSoci.xlsx");
+	$spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load("TemplateExport".$title.".xlsx"); //0
 	include_once("database/databaseii.php");
 
-	$file_name = date('Ymd_h.i.s').'_AnagraficaSoci';
 
-	$campo =  array(19);
+	$file_name = date('Ymd_h.i.s').'_'.$title;
 
-	//per foglio famiglie
+	$colonna = ["idle", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ", "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ", "CA", "CB", "CC", "CD", "CE", "CF", "CG", "CH", "CI", "CJ", "CK", "CL", "CM", "CN", "CO", "CP", "CQ", "CR", "CS", "CT", "CU", "CV", "CW", "CX", "CY", "CZ"];
+
+	$numcampi = count($nomiCampiA);
+
+	for ($c = 1; $c < $numcampi; $c++) {
+		$campiSQL = $campiSQL.", ".$nomiCampiA[$c];
+	}
+
+	$campiSQL = substr($campiSQL, 2); 
+
+	//4, 5
 	$sql = "SELECT 
-	tipo_soc, dataiscrizione_soc, cognome_soc, nome_soc, telefono_soc, altrotel_soc, email_soc, 
-	datanascita_soc, comunenascita_soc, provnascita_soc, paesenascita_soc, cf_soc, indirizzo_soc, comune_soc, CAP_soc, prov_soc, paese_soc
-	FROM tab_anagraficasoci
-	ORDER BY cognome_soc";
+	".$campiSQL."
+	FROM ". $from."
+	ORDER BY ".$orderBY;
 	$stmt = mysqli_prepare($mysqli, $sql);
 	mysqli_stmt_execute($stmt);
-	mysqli_stmt_bind_result($stmt, $campo[1], $campo[2], $campo[3], $campo[4], $campo[5], $campo[6], $campo[7], $campo[8], $campo[9], $campo[10], $campo[11], $campo[12], $campo[13], $campo[14], $campo[15], $campo[16], $campo[17]); 
-	$colonna = ["idle", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ", "BA", "BB", "BC", "BD", "BE", "BF", "BG", "BH", "BI", "BJ", "BK", "BL", "BM", "BN", "BO", "BP", "BQ", "BR", "BS", "BT", "BU", "BV", "BW", "BX", "BY", "BZ", "CA", "CB", "CC", "CD"];
 
+	$resultFields = array(&$stmt);
 
-	$spreadsheet->setActiveSheetIndex(0); // Comincia da 0
+	$campo =  array($numcampi);
+	for ($c = 1; $c < $numcampi; $c++) {
+		$resultFields[$c] = & $campo[$c]; 
+	}
+	call_user_func_array ("mysqli_stmt_bind_result", $resultFields);
+	$stmt->execute();
+
+	$spreadsheet->setActiveSheetIndex(0);
 	
+	//6
+	$spreadsheet->getActiveSheet()->SetCellValue("A1", $title." al ".date('d/m/Y'));
 
-	$spreadsheet->getActiveSheet()->SetCellValue("B1", "Elenco Soci al ".date('d/m/Y'));
-
-	$j = 5; //j corrisponde al numero di riga di excel
 	$date1=date_create("1900-01-01");
-	$tiposocA = array(0=>"Fruitore", 1=>"Lavoratore",  2=>"Volontario", 3=>"Altro");
 
+	$j = 5;
 	while (mysqli_stmt_fetch($stmt)) { 
 		$j++;
 		$spreadsheet->getActiveSheet()->SetCellValue("A".$j, $j-5 );
-		for ($x = 1; $x <= 17; $x++) {
-
-			switch ($x) {
-				case 1 :
-					$spreadsheet->getActiveSheet()->SetCellValue($colonna[$x].$j, $tiposocA[$campo[$x]] );
-					break;
-				case 2 :
-				case 8 :
-					$date2=date_create($campo[$x]);
-					$diff=date_diff($date1,$date2);
-					$days=$diff->format("%a");
-					$days+=2; // add boundary days
-					$spreadsheet->getActiveSheet()->setCellValue($colonna[$x].$j,$days); 
-					break;
-				default:
-					$spreadsheet->getActiveSheet()->SetCellValue($colonna[$x].$j, $campo[$x] );
-
-					break;
+		for ($x = 1; $x < $numcampi; $x++) {
+			if ($dataNonDataA[$x] == 1) {
+				$date2=date_create($campo[$x]);
+				$diff=date_diff($date1,$date2);
+				$days=$diff->format("%a");
+				$days+=2;
+				$spreadsheet->getActiveSheet()->setCellValue($colonna[$x].$j,$days); 
+			} else {
+				$spreadsheet->getActiveSheet()->SetCellValue($colonna[$x].$j, $campo[$x] );
 			}
-
-
-
 		}
 	}
 
