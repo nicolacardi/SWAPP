78a0f5f0fc6b33febef1f8c7a93f9ff01524056f
nicolacardi <nicola.cardi@gmail.com>
2023-01-13 21:52:36 +0100

    ...

--- iscrizioni/inc_pattocorresponsabilitaModuloIscrizione.php
+++ iscrizioni/inc_pattocorresponsabilitaModuloIscrizione.php
@@ -226,8 +226,8 @@
         $pdf->Cell(60,5,"Firma del padre/tutore* (leggibile)",0,0,'C');
         $pdf->Cell(5,5,"",0,0,'C');
         $pdf->Cell(60,5,"Firma della madre/tutrice* (leggibile)",0,0,'C');
-        $pdf->Cell(5,5,"",0,0,'C');
-        $pdf->Cell(60,5,"L'alunno/a",0,1,'C');
+        $pdf->Cell(5,5,"",0,1,'C');
+        //$pdf->Cell(60,5,"L'alunno/a",0,1,'C');
 
         $pdf->SetFont($fontdefault,'',8);
         $pdf->Cell(60,5,"(* ove presente)",0,0,'C');
@@ -240,11 +240,11 @@
         $pdf->Cell(5,5,"","",0,'C');
         $pdf->Cell(60,5,"","B",0,'C');
         $pdf->Cell(5,5,"","",0,'C');
-        $pdf->Cell(60,5,"","B",0,'C');
+        //$pdf->Cell(60,5,"","B",0,'C');
         
         $pdf->Image('../assets/img/Icone/frecciafirmablack.png', $pdf->GetX()-200, $pdf->GetY()-18, 20);
         $pdf->Image('../assets/img/Icone/frecciafirmablack.png', $pdf->GetX()-135, $pdf->GetY()-18, 20);
-        $pdf->Image('../assets/img/Icone/frecciafirmablack.png', $pdf->GetX()-60, $pdf->GetY()-18, 20);
+        //$pdf->Image('../assets/img/Icone/frecciafirmablack.png', $pdf->GetX()-60, $pdf->GetY()-18, 20);
         
     }
 
