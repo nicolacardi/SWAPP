49fd7f485e276e728e2f5a77301ecb9eb1106d7f
nicolacardi <nicola.cardi@gmail.com>
2023-03-07 12:41:38 +0100

    ...

--- database/databaseBii.php
+++ database/databaseBii.php
@@ -2,19 +2,19 @@
 
 session_start();
 
-$_SESSION['databaseB'] = 'swappwaldorfB_arca';
-$_SESSION['databaseA'] = 'swappwaldorf_arca';
-$codscuola="AR";
+$_SESSION['databaseB'] = 'swappwaldorfB_trento';
+$_SESSION['databaseA'] = 'swappwaldorf_trento';
+$codscuola="TN";
 
 // Variabili per accesso al database
-//$username = "Sql1639091";
-//$password = "NickCards2!";
-//$database = "Sql1639091_3";
-//$host = "89.46.111.235";
+//$username = "Sql1592916";
+//$password = "LUR$YkgD76";
+//$database = "Sql1592916_2";
+//$host = "31.11.39.64";
 
 $username = 'root';
 $password = 'root';
-$database = 'swappwaldorfB_arca';
+$database = 'swappwaldorfB_trento';
 $host = 'localhost';
 
 //lavoro in forma procedurale con la libreria mysqli
